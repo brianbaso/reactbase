0fde995e2be22b50d5d78d70e9afb8937e67020a
Brian <brianbaso@gmail.com>
2019-07-31 16:45:53 -0700

    firebase config

--- src/index.js
+++ src/index.js
@@ -3,7 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import * as firebase from "firebase/app";
+import "firebase/auth";
 
+var firebaseConfig = {
+  apiKey: "AIzaSyAfS28mDMCHA-5iZw22CbOJumzdOFS-1f0",
+  authDomain: "reactbase-b7ba1.firebaseapp.com",
+  databaseURL: "https://reactbase-b7ba1.firebaseio.com",
+  projectId: "reactbase-b7ba1",
+  storageBucket: "reactbase-b7ba1.appspot.com",
+  messagingSenderId: "189653701259",
+  appId: "1:189653701259:web:9b28cb22de9f8545"
+};
+
+firebase.initializeApp(firebaseConfig);
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
